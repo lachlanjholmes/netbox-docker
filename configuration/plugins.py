2172e592bc765abf37cac2e5c6812c0c9629a5ec
lachlanjholmes <1935116+lachlanjholmes@users.noreply.github.com>
2025-06-15 12:42:54 +1000

    Update plugins.py

--- configuration/plugins.py
+++ configuration/plugins.py
@@ -4,8 +4,8 @@
 # To learn how to build images with your required plugins
 # See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
 
-# PLUGINS = ["netbox_bgp"]
+PLUGINS = ["slurpit_netbox"]
 
-PLUGINS_CONFIG = {
-  "slurpit_netbox",
-}
+# PLUGINS_CONFIG = {
+#   "slurpit_netbox",
+# }
